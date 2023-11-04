ffbdd616b8f32549745dcf31812a9a72e17287be
Muhammad Fiqri Firmansyah <mfiqrifirmansyah.92@gmail.com>
2023-11-04 10:04:39 +0700

    Latihan

---
+++ Tugas Struktur Kondisi/Latihan2.py
@@ -0,0 +1,11 @@
+bil1 = int(input("Bilangan ke-1: "))
+bil2 = int(input("Bilangan ke-2: "))
+bil3 = int(input("Bilangan ke-3: "))
+
+#Buat variable data
+data = [bil1, bil2, bil3]
+
+#Menampilkan data
+print("Data sebelum di urutkan :", data)
+list.sort(data)
+print("Data setelah di urutkan :", data)
